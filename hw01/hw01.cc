c0cf1946189a2d0ddf7f839af159f1dc57d24bbd
Rommela Marie Dimaunahan <melah0418@gmail.com>
2019-09-04 15:42:58 -1000

    Homework1

--- hw01/hw01.cc
+++ hw01/hw01.cc
@@ -1,11 +1,14 @@
 #include <cstdio>
-#include "factorial.h"
+#include "cubed.h"
 
-
-int main (int argc , char** argv){
-	printf("Enter integer to know its factorial);
+int main(int argc , char** argv){
+	printf("Enter integer to know its cubed value");
 	int n;
 	scanf("%d", &n);
-	printf("The integer's factorial is %d", factorial(n));
+	printf("The integer's cube is %d", cubed(n));
 }	
+
+int cubed(int num){
+	return num*num*num;
+}
 
